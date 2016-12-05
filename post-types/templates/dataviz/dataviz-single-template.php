d276617f559f4a9b4561276d3693ce7ceaf9846b
Alex Corbi <a.corbi@gmail.com>
2016-12-05 18:03:08 +0100

    Fixes #59

--- post-types/templates/dataviz/dataviz-single-template.php
+++ post-types/templates/dataviz/dataviz-single-template.php
@@ -6,9 +6,9 @@ $resource_id = get_post_meta($post_id, '_ckan_resource_id', true);
 $resource_link = get_post_meta($post_id, '_ckan_resource_download_link', true);
 $resource_filter = get_post_meta($post_id, '_ckan_resource_filter', true);
 $viz_type = get_post_meta($post_id, '_viz_type', true);
-$viz_options = get_post_meta($post_id, '_viz_options', true);
-$viz_field_ids = get_post_meta($post_id, '_viz_field_ids', true);
-$viz_columns = get_post_meta($post_id, '_viz_columns', true);
+$viz_options = odm_language_manager()->get_the_language_by_site() !== 'English' ? get_post_meta($post_id, '_viz_options_localization', true) : get_post_meta($post_id, '_viz_options', true);
+$viz_field_ids = odm_language_manager()->get_the_language_by_site() !== 'English' ? get_post_meta($post_id, '_viz_field_ids_localization', true) : get_post_meta($post_id, '_viz_field_ids', true);
+$viz_columns = odm_language_manager()->get_the_language_by_site() !== 'English' ? get_post_meta($post_id, '_viz_columns_localization', true) : get_post_meta($post_id, '_viz_columns', true);
 
 if (isset($atts["width"])) {
 	$viz_width = $atts["width"];
@@ -27,10 +27,10 @@ if (isset($atts["height"])) {
 <div class="chart_wrapper" style="width:<?php echo $viz_width; ?>;">
 	<?php if (is_single($post_id)): ?>
 		<h2><?php echo $post->post_title; ?></h2>
-	<?php else: ?>	
+	<?php else: ?>
 		<h2><a href="<?php echo get_permalink($post_id); ?>"><?php echo $post->post_title; ?></a></h2>
 	<?php endif ?>
-	
+
 	<?php
 		if (isset($atts["hide_description"]) && filter_var($atts["hide_description"], FILTER_VALIDATE_BOOLEAN) === true) {
 			echo "";
@@ -62,12 +62,12 @@ if (isset($atts["height"])) {
 	}
 
 	var chart_<?php echo $post_id; ?> = new ODChart(config);
-	
+
 	jQuery(document).ready(function(){
 		google.charts.load('current', {'packages':['corechart', 'table', 'treemap']});
 		google.charts.setOnLoadCallback(initChart<?php echo $post_id; ?>);
 	});
-	
+
 	function initChart<?php echo $post_id; ?>()
 	{
 		chart_<?php echo $post_id; ?>.init();
