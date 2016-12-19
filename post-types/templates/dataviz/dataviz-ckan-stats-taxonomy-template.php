9d3a665dc1373f4f07ac6595348c7a08148b9379
Alex Corbi <a.corbi@gmail.com>
2016-12-19 19:28:29 +0100

    adapted stats-taxonomy template so its pulled from stats/ folder

--- post-types/templates/dataviz/dataviz-ckan-stats-taxonomy-template.php
+++ post-types/templates/dataviz/dataviz-ckan-stats-taxonomy-template.php
@@ -9,64 +9,54 @@
   style="width: 100%; height: 200px;">
 </div>
 
+<div id="ckan-stats-filter"></div>
+
 <script type="text/javascript">
-	google.charts.load("current", {packages:["corechart"]});
+  
+	google.charts.load("current", {packages:["treemap"]});
 	google.charts.setOnLoadCallback(drawChart);
-
-	function getNumberOfRecordsByTaxonomy(taxonomy){
-		var ckan_domain = $('#ckan-stats-taxonomy').data('ckan-domain');
-    var current_country_code = $('#ckan-stats-taxonomy').data('current-country-code');
-		var request_url = ckan_domain + '/api/3/action/package_search?fq=extras_taxonomy:"' + taxonomy + '"';
-    var type = $('#ckan-stats-taxonomy').data('type');
-
-    if (current_country_code !== 'mekong'){
-      request_url = request_url + '+extras_odm_spatial_range:' + current_country_code;
-    }
-
-    if (type){
-      request_url = request_url + '+type:' + type;
-    }
-
-		var request = new XMLHttpRequest();
-		request.open('GET', request_url, false);  // `false` makes the request synchronous
-		request.send(null);
-
-		if (request.status === 200) {
-		  var json_response = JSON.parse(request.responseText);
-			return json_response['result']['count'];
-		}
-	};
-
+  
 	function drawChart() {
+    
+    var taxonomy_definition_url = "stats/records_by_taxonomy.json";  
+        
+    $.ajax({
+       type: 'GET',
+       url: taxonomy_definition_url,
+       dataType:'json',
+       success: function(data) { 
+                
+        var chartData = new google.visualization.DataTable();
+        chartData.addColumn('string', 'Topic');
+        chartData.addColumn('string', 'Parent');
+        chartData.addColumn('number', 'Number of records');
+        chartData.addRows(data["config"]);
+       
+     		var options = {
+          legend: { position: "none" },
+          highlightOnMouseOver: true
+        };
 
-		var data = google.visualization.arrayToDataTable([
-			['Topic', 'Number of records', { role: 'style' }],
-			['Agriculture and fishing',     						getNumberOfRecordsByTaxonomy('Agriculture and fishing'), 'color: #c8c8c8'],
-			['Aid and development',     								getNumberOfRecordsByTaxonomy('Aid and development'), 'color: #c8c8c8'],
-			['Disasters and emergency response',     		getNumberOfRecordsByTaxonomy('Disasters and emergency response'), 'color: #c8c8c8'],
-			['Economy and commerce',     								getNumberOfRecordsByTaxonomy('Economy and commerce'), 'color: #c8c8c8'],
-			['Energy',     															getNumberOfRecordsByTaxonomy('Energy'), 'color: #c8c8c8'],
-			['Environment and natural resources',     	getNumberOfRecordsByTaxonomy('Environment and natural resources'), 'color: #c8c8c8'],
-			['Extractive industries',     							getNumberOfRecordsByTaxonomy('Extractive industries'), 'color: #c8c8c8'],
-			['Government',     													getNumberOfRecordsByTaxonomy('Government'), 'color: #c8c8c8'],
-			['Industries',     													getNumberOfRecordsByTaxonomy('Industries'), 'color: #c8c8c8'],
-			['Infrastructure',     											getNumberOfRecordsByTaxonomy('Infrastructure'), 'color: #c8c8c8'],
-			['Labor',     															getNumberOfRecordsByTaxonomy('Labor'), 'color: #c8c8c8'],
-			['Land',     																getNumberOfRecordsByTaxonomy('Land'), 'color: #c8c8c8'],
-			['Law and judiciary',     									getNumberOfRecordsByTaxonomy('Law and judiciary'), 'color: #c8c8c8'],
-			['Population and censuses',     						getNumberOfRecordsByTaxonomy('Population and censuses'), 'color: #c8c8c8'],
-			['Social development',     									getNumberOfRecordsByTaxonomy('Social development'), 'color: #c8c8c8'],
-			['Urban administration and development',    getNumberOfRecordsByTaxonomy('Urban administration and development'), 'color: #c8c8c8'],
-			['Science and technology',     							getNumberOfRecordsByTaxonomy('Science and technology'), 'color: #c8c8c8']
-		]);
-
-		var options = {
-      title: "Number of records by topic",
-      legend: { position: "none" }
-    };
-
-		var chart = new google.visualization.ColumnChart(document.getElementById('ckan-stats-taxonomy'));
-		chart.draw(data, options);
+     		var chart = new google.visualization.TreeMap(document.getElementById('ckan-stats-taxonomy'));     		
+        
+        function selectHandler() {
+          var selectedItem = chart.getSelection()[0];
+          if (selectedItem) {
+            var value = chartData.getValue(selectedItem.row, 0);            
+            $('#ckan-stats-filter').html('<a href="?query=&taxonomy=' + encodeURIComponent(value) + '">Filter only records about ' + value + '</a>')
+          }
+        }
+        
+        google.visualization.events.addListener(chart, 'select', selectHandler);
+        chart.draw(chartData, options);
+      
+       },
+       error: function(error){
+         console.log("error", error);
+       },
+       async: false
+   }); 
+  	
 	};
 
 </script>
