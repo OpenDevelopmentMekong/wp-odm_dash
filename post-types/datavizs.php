e318bcde7625b7156ab1ac42c72d4f0ba7afe543
Alex Corbi <a.corbi@gmail.com>
2017-06-05 16:04:51 +0200

    fixing layout

--- post-types/datavizs.php
+++ post-types/datavizs.php
@@ -186,7 +186,7 @@ if (!class_exists('Odm_DataViz_Post_Type')) {
                       <label for="_custom_data">Custom data :</label>
                     </td>
                     <td>
-                      <textarea id="_custom_data" name="_custom_data" placeholder="Enter data in proper format here" value="<?php echo $custom_data ?>" style="width:100%;" />
+                      <textarea id="_custom_data" name="_custom_data" placeholder="Enter data in proper format here" value="<?php echo $custom_data; ?>" style="width:100%;" />
                       <p class="description">
                         Read guide on https://developers.google.com/chart/interactive/docs/basic_preparing_data</br>
                         Example : [
@@ -198,11 +198,10 @@ if (!class_exists('Odm_DataViz_Post_Type')) {
                         ]
                       </p>
                     </td>
-                  </tr>                  
+                  </tr>
                 </tbody>
               </table>
             </div>
-          </div>
 
         <?php
         }
