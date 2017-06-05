a975351324f477b00d9f70c195e0b73dbe5a6181
Alex Corbi <a.corbi@gmail.com>
2017-06-05 21:52:02 +0200

    increased version number

--- wpdash.php
+++ wpdash.php
@@ -3,7 +3,7 @@
  * Plugin Name: ODM Dashboards
  * Plugin URI: http://github.com/OpenDevelopmentMekong/wpdash
  * Description: Internal wordpress plugin for exposing widgets with data visualizations.
- * Version: 2.1.4
+ * Version: 2.1.5
  * Author: Alex Corbi (mail@lifeformapps.com)
  * Author URI: http://www.lifeformapps.com
  * License: GPLv3.
